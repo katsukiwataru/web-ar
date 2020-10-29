4589cb3f5abb8a31fbac38d7f264b7f2c37859b1
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-10-29 23:39:49 +0900

    feat: rename ARScript

--- src/components/Helmet/ARScript.tsx
+++ src/components/Helmet/ARScript.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-export const Script = () => {
+export const ARScript = () => {
   return (
     <Helmet>
       <script defer src="https://raw.githack.com/AR-js-org/AR.js/master/three.js/build/ar-nft.js"></script>
