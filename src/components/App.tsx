87097202e71bab76f4248b22eac6f0b43d490ab5
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-10-31 11:07:57 +0900

    remove: remove import three

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { ThreeAR } from './ThreeAR/ThreeAR';
-import * as THREE from 'three';
-
-window.THREE = THREE;
 
 export const App = () => {
   return <ThreeAR />;
