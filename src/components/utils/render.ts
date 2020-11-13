24e7adbe3e38a52e4b8666742c948516aee180e1
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-11-13 18:06:11 +0900

    feat: split to THREEx

--- src/components/utils/render.ts
+++ src/components/utils/render.ts
@@ -1,27 +1,65 @@
-import { camera } from '../camera';
 import { webGLRenderer } from '../renderer/webGLRenderer';
-import { scene } from '../scene';
-import { arToolkitContext, arToolkitSource } from '../THREEx';
+import { arToolkitContext, arToolkitSource, onResize } from '../THREEx';
 
-export function renderSample(el: HTMLElement) {
+export function render(el: HTMLElement) {
   el.appendChild(webGLRenderer.domElement);
-  // need to declare CubeGeometry type
-  // @ts-ignore
-  const cube = new THREE.CubeGeometry(1, 1, 1);
-  const mesh = new THREE.Mesh(cube, new THREE.MeshNormalMaterial());
-  mesh.position.y = 1.0;
-  scene.add(mesh);
 
-  const clock = new THREE.Clock();
+  window.addEventListener('resize', () => {
+    onResize();
+  });
+
+  arToolkitSource.init(() => {
+    setTimeout(() => {
+      onResize();
+    }, 1000);
+  });
+
+  arToolkitContext.init(() => {
+    camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix());
+  });
+
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera();
+  scene.add(camera);
+
+  const group = new THREE.Group();
+  scene.add(group);
+
+  new THREEx.ArMarkerControls(arToolkitContext, group, {
+    type: 'pattern',
+    patternUrl: 'data/orca.patt',
+    changeMatrixMode: 'modelViewMatrix',
+  });
+
+  const markerPlane = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(1, 1),
+    new THREE.MeshBasicMaterial({
+      colorWrite: false,
+      depthWrite: false,
+    }),
+  );
+  markerPlane.rotation.x = -0.5 * Math.PI;
+  group.add(markerPlane);
+
+  const loader = new THREE.FontLoader();
+  loader.load('fonts/helvetiker_regular.typeface.json', (font) => {
+    const textGeom = new THREE.TextBufferGeometry('Tap Marker!', {
+      font: font,
+      size: 0.2,
+      height: 0.04,
+    });
+    textGeom.center();
+    const text = new THREE.Mesh(textGeom, new THREE.MeshNormalMaterial());
+    text.position.set(0, 0.75, 0);
+    group.add(text);
+  });
+
   const animate = () => {
     requestAnimationFrame(animate);
     if (arToolkitSource.ready) {
       arToolkitContext.update(arToolkitSource.domElement);
       scene.visible = camera.visible;
     }
-    const delta = clock.getDelta();
-    mesh.rotation.x += delta * 1.0;
-    mesh.rotation.y += delta * 1.5;
     webGLRenderer.render(scene, camera);
   };
   requestAnimationFrame(animate);
