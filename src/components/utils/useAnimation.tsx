deeaeb9503068aa1ea69cd4747c494c82dccc784
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-11-20 16:55:55 +0900

    feat: change to useAnimation

--- src/components/utils/useAnimation.tsx
+++ src/components/utils/useAnimation.tsx
@@ -6,17 +6,22 @@ import { arToolkitContext, arToolkitSource } from '../THREEx';
 
 export const useAnimationFrame = (webGLRenderer: WebGLRenderer | null) => {
   const requestRef = useRef(0);
-  const animate = () => {
-    if (arToolkitSource.ready) {
-      arToolkitContext.update(arToolkitSource.domElement);
-      scene.visible = perspectiveCamera.visible;
-    }
-    if (!webGLRenderer) return;
-    webGLRenderer.render(scene, perspectiveCamera);
-    requestRef.current = requestAnimationFrame(animate);
-  };
 
   useEffect(() => {
+    const animate = () => {
+      if (arToolkitSource.ready) {
+        arToolkitContext.update(arToolkitSource.domElement);
+        scene.visible = perspectiveCamera.visible;
+      }
+
+      // const intersects = raycaster.intersectObjects(scene.children);
+      // for (let i = 0; i < intersects.length; i++) {
+      //   console.log(intersects[i].object);
+      // }
+      if (!webGLRenderer) return;
+      webGLRenderer.render(scene, perspectiveCamera);
+      requestRef.current = requestAnimationFrame(animate);
+    };
     requestRef.current = requestAnimationFrame(animate);
     return () => cancelAnimationFrame(requestRef.current);
   }, [webGLRenderer]);
