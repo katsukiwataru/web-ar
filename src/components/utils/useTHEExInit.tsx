c8182f992062359658f9f966d9bd97768d51221a
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-11-27 02:31:48 +0900

    feat: move ArMarkerControls out of ThrexInit

--- src/components/utils/useTHEExInit.tsx
+++ src/components/utils/useTHEExInit.tsx
@@ -1,15 +1,8 @@
 import { useEffect } from 'react';
 import { arToolkitContext, arToolkitSource, onResize } from '../THREEx';
-import { group } from '../group';
 import { perspectiveCamera } from '../camera';
 
 export const ThreexInit = () => {
-  new THREEx.ArMarkerControls(arToolkitContext, group, {
-    type: 'pattern',
-    patternUrl: 'data/orca.patt',
-    changeMatrixMode: 'modelViewMatrix',
-  });
-
   useEffect(() => {
     arToolkitContext.init(() => {
       perspectiveCamera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix());
