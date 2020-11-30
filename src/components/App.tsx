7ac93e2df72f8f5c7d8aa6b94054fe94d6719abd
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-12-01 01:34:02 +0900

    feat: comment out

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { RootComponent } from './Three/RootComponent';
-// import { ThreeComponent } from './Three/ThreeComponent';
-// import { Three } from './sample';
 
 export const App = () => {
   return (
@@ -12,9 +10,6 @@ export const App = () => {
           <Route exact path="/">
             <RootComponent />
           </Route>
-          {/* <Route exact path="/three">
-            <ThreeComponent />
-          </Route> */}
         </Switch>
       </BrowserRouter>
     </>
