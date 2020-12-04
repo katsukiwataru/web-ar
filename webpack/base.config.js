2edd811c0c56c94610b59f1006c8c2b4f6688aea
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-12-04 16:39:27 +0900

    feat: add reqire webpack

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
