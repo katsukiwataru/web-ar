b3ccd578d747c9a2ce8b49c4bd1cd4d9ba2e0f70
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-12-12 05:45:35 +0900

    feat: remove commentout

--- webpack/develop.config.js
+++ webpack/develop.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-// const webpack = require('webpack');
 const ip = require('ip');
 const { merge } = require('webpack-merge');
 const base = require('./base.config');
@@ -8,13 +7,6 @@ const root = path.resolve(__dirname, '../');
 
 module.exports = merge(base, {
   mode: 'development',
-  // plugins: [
-  //   new webpack.DefinePlugin({
-  //     'process.env': {
-  //       NODE_ENV: JSON.stringify('development'),
-  //     },
-  //   }),
-  // ],
   devtool: 'source-map',
   devServer: {
     open: true,
