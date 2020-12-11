dd4998483c39ee7b71987ba505fe4d19c731d7c1
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-12-12 05:46:51 +0900

    feat: add fetch option

--- src/lib/api/base.ts
+++ src/lib/api/base.ts
@@ -1,11 +1,14 @@
-const API_BASE_URL = localStorage.getItem('API_BASE_URL') || 'http://localhost:3000';
+const API_BASE_URL = localStorage.getItem('API_BASE_URL') || 'https://localhost:3000';
 
 window.console.log(`API_BASE_URL: ${API_BASE_URL}`);
 
 export const apiURL = new URL(`${API_BASE_URL}/api/v1/`);
 
 export const apiBase = async (url: URL) => {
-  const res = await fetch(url.href);
+  const res = await fetch(url.href, {
+    method: 'GET',
+    mode: 'no-cors',
+  });
   const data = await res.json();
   return data;
 };
