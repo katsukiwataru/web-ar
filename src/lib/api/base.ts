5e7cc17bcb9224cca514d4e827f3f86204bbcb43
whatasoda <git@whatasoda.me>
2020-12-12 02:08:59 +0900

    refactor: configurable api base url

--- src/lib/api/base.ts
+++ src/lib/api/base.ts
@@ -1,4 +1,8 @@
-export const apiURL = new URL(`http://localhost:3000/api/v1/`);
+const API_BASE_URL = localStorage.getItem('API_BASE_URL') || 'http://localhost:3000';
+
+window.console.log(`API_BASE_URL: ${API_BASE_URL}`);
+
+export const apiURL = new URL(`${API_BASE_URL}/api/v1/`);
 
 export const apiBase = async (url: URL) => {
   const res = await fetch(url.href);
