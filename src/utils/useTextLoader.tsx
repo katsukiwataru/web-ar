0f7e9279c55cc940cdbc8211f7a305d5cfe849ab
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-12-14 13:45:12 +0900

    feat: add text

--- src/utils/useTextLoader.tsx
+++ src/utils/useTextLoader.tsx
@@ -1,19 +1,19 @@
 import { useEffect } from 'react';
 import typeface from '../../fonts/helvetiker_regular.typeface';
 
-export const useTextLoader = (group: THREE.Group, pathName: string) => {
+export const useTextLoader = (group: THREE.Group, text: string, y: number) => {
   useEffect(() => {
     const loader = new THREE.FontLoader();
     loader.load(typeface, (font) => {
-      const textGeom = new THREE.TextBufferGeometry(`${pathName}`, {
+      const textGeom = new THREE.TextBufferGeometry(`${text}`, {
         font,
         size: 0.2,
         height: 0.04,
       });
       textGeom.center();
-      const text = new THREE.Mesh(textGeom, new THREE.MeshNormalMaterial());
-      text.position.set(0, 0.75, 0);
-      group.add(text);
+      const textMesh = new THREE.Mesh(textGeom, new THREE.MeshNormalMaterial());
+      textMesh.position.set(0, y, 0);
+      group.add(textMesh);
     });
-  }, [group, pathName]);
+  }, [group, text, y]);
 };
