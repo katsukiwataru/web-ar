eac98279bae5ce69e333264f9fe65f547c46f1e8
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-12-16 17:12:36 +0900

    feat: change to string[] and fix position

--- src/components/pages/user/UserComponent.tsx
+++ src/components/pages/user/UserComponent.tsx
@@ -74,8 +74,7 @@ export const UserComponent = memo(() => {
     return user ? [twitterScreenName, new Date(user.created_at).toLocaleString('ja-jp')] : [twitterScreenName, ''];
   }, [user]);
 
-  useTextLoader(group, textLoader[0], 0.75);
-  useTextLoader(group, textLoader[1], 0.5);
+  useTextLoader(group, textLoader, 0.75);
   // usePlaneMesh(group, textCanvasRef);
 
   useAnimationFrame({ arToolkitSource, arToolkitContext, webGLRenderer, scene, perspectiveCamera });
--- src/utils/useTextLoader.tsx
+++ src/utils/useTextLoader.tsx
@@ -1,19 +1,21 @@
 import { useEffect } from 'react';
 import typeface from '../../fonts/helvetiker_regular.typeface';
 
-export const useTextLoader = (group: THREE.Group, text: string, y: number) => {
+export const useTextLoader = (group: THREE.Group, texts: string[]) => {
   useEffect(() => {
     const loader = new THREE.FontLoader();
-    loader.load(typeface, (font) => {
-      const textGeom = new THREE.TextBufferGeometry(`${text}`, {
-        font,
-        size: 0.2,
-        height: 0.04,
+    texts.map((text, index) => {
+      loader.load(typeface, (font) => {
+        const textGeom = new THREE.TextBufferGeometry(`${text}`, {
+          font,
+          size: 0.5,
+          height: 0.04,
+        });
+        textGeom.center();
+        const textMesh = new THREE.Mesh(textGeom, new THREE.MeshNormalMaterial());
+        textMesh.position.set(-index, -index, -3);
+        group.add(textMesh);
       });
-      textGeom.center();
-      const textMesh = new THREE.Mesh(textGeom, new THREE.MeshNormalMaterial());
-      textMesh.position.set(0, y, 0);
-      group.add(textMesh);
     });
-  }, [group, text, y]);
+  }, [group, texts]);
 };
