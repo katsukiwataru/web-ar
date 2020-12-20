900e4508a7ac4b582691d87da2ce0cbc04ea3468
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-12-20 21:44:40 +0900

    feat: remove webpack and setup next.config

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  webpack: (config, { webpack }) => {
+    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
+    return config;
+  },
 };
