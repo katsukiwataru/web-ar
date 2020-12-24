190afc736df191089ce2e1d6453790a9d67d4b68
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-12-25 03:41:08 +0900

    feat: add loadable and fix router

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,13 @@
-import React, { lazy, Suspense } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from 'react';
 import { UserContextProvider, UserMarkerContextProvider } from './lib/context';
-
-const Top = lazy(() =>
-  import(
-    /* webpackChunkName: "top" */
-    './components/pages/top/TopComponent'
-  ).then((module) => ({ default: module.TopComponent })),
-);
-const User = lazy(() =>
-  import(
-    /* webpackChunkName: "user" */
-    './components/pages/user/UserComponent'
-  ).then((module) => ({ default: module.UserComponent })),
-);
-const Marker = lazy(() =>
-  import(
-    /* webpackChunkName: "user" */
-    './components/pages/marker/MarkerComponent'
-  ).then((module) => ({ default: module.MarkerComponent })),
-);
+import { Router } from './lib/router/route';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <UserContextProvider>
-          <UserMarkerContextProvider>
-            <Suspense fallback={<div>loading</div>}>
-              <Route exact path="/" component={Top} />
-              <Route exact path="/user/:screenName" component={User} />
-              <Route exact path="/user/marker/:screenName" component={Marker} />
-            </Suspense>
-          </UserMarkerContextProvider>
-        </UserContextProvider>
-      </Switch>
-    </BrowserRouter>
+    <UserContextProvider>
+      <UserMarkerContextProvider>
+        <Router />
+      </UserMarkerContextProvider>
+    </UserContextProvider>
   );
 };
