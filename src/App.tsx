8bf4d9ba9f04c2d30f7cf9d97540f12245159024
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-12-26 15:34:15 +0900

    feat: rename

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { UserContextProvider, UserMarkerContextProvider } from './lib/context';
+import { MarkerContextProvider, UserContextProvider } from './lib/context';
 import { Router } from './lib/router/route';
 
 export const App = () => {
   return (
     <UserContextProvider>
-      <UserMarkerContextProvider>
+      <MarkerContextProvider>
         <Router />
-      </UserMarkerContextProvider>
+      </MarkerContextProvider>
     </UserContextProvider>
   );
 };
