a1cb7545d51895dac69b7e7b0204442b73499e28
Wataru Katsuki <a17dc050@dhw.ac.jp>
2021-01-08 21:53:08 +0900

    feat: fix type

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,12 +1,11 @@
-import type Three from 'three';
+import type three from 'three';
 import type THREEx from './THREEx';
 
 declare global {
-  const THREE: typeof Three;
+  const THREE: typeof three;
   const THREEx: THREEx;
-
   interface Window {
     THREE?: typeof THREE;
-    THREEx?: typeof THREEx;
+    THREEx?: THREEx;
   }
 }
