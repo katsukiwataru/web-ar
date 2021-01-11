d30484768fe1ba083d93c0f61b484d248832a3f3
Wataru Katsuki <a17dc050@dhw.ac.jp>
2021-01-12 03:00:11 +0900

    feat: split text

--- src/utils/useTextLoader.tsx
+++ src/utils/useTextLoader.tsx
@@ -4,16 +4,16 @@ export const useTextLoader = (res: TwitterUserFavorite[] | null) => {
   const favolites = useMemo(() => {
     if (!res) return;
     return res.slice(0, 1).map((r) => {
-      const userName = `@${r.user.name}`;
       const httpsIndex = r.text.indexOf('https');
       const text = r.text.slice(0, httpsIndex);
+      const resultText = text.split(/(.{10})/).filter((e) => e);
       return new THREE.TextSprite({
         alignment: 'left',
         color: '#24ff00',
         fontFamily: '"Times New Roman", Times, serif',
         fontSize: 0.5,
         fontStyle: 'italic',
-        text: [userName, text].join('\n'),
+        text: resultText.join('\n'),
       });
     });
   }, [res]);
