bee1a116b04c3fc92c1e983519d90bc4554342eb
Wataru Katsuki <a17dc050@dhw.ac.jp>
2021-01-19 16:58:09 +0900

    feat: add origin

--- src/lib/api/base.ts
+++ src/lib/api/base.ts
@@ -1,4 +1,8 @@
-const API_BASEURL = 'https://192.168.11.101:3001/api/v1';
+// const API_BASEURL = 'https://192.168.11.101:3001/api/v1';
+const origin =
+  process.env.MODE === 'development' ? 'https://172.20.0.38:3001' : 'https://secure-ridge-14179.herokuapp.com/api/v1/';
+
+const API_BASEURL = `${origin}/api/v1`;
 
 export const apiBase = async (path: string, query?: string) => {
   const res = await fetch(`${API_BASEURL}/${path}?${query}`);
