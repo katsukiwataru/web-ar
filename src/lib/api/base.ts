6a9070b5722664644565b9bc3a99706d5e9736dd
Wataru Katsuki <a17dc050@dhw.ac.jp>
2021-01-19 17:07:41 +0900

    fix: heroku url

--- src/lib/api/base.ts
+++ src/lib/api/base.ts
@@ -1,6 +1,6 @@
 // const API_BASEURL = 'https://192.168.11.101:3001/api/v1';
 const origin =
-  process.env.MODE === 'development' ? 'https://172.20.0.38:3001' : 'https://secure-ridge-14179.herokuapp.com/api/v1/';
+  process.env.MODE === 'development' ? 'https://172.20.0.38:3001' : 'https://secure-ridge-14179.herokuapp.com';
 
 const API_BASEURL = `${origin}/api/v1`;
 
