aa98ef6c9d1294120876c010606f9615d839a102
Wataru Katsuki <a17dc050@dhw.ac.jp>
2021-02-09 03:54:18 +0900

    feat: split origin to dev and pro

--- src/lib/api/base.ts
+++ src/lib/api/base.ts
@@ -1,10 +1,10 @@
-const DEV_ORIGIN = 'https://172.20.0.38:3001';
+const DEV_ORIGIN = 'https://192.168.11.101:3001';
 
 const PRO_ORIGIN = 'https://secure-ridge-14179.herokuapp.com';
 
-const origin = process.env.MODE === 'development' ? DEV_ORIGIN : PRO_ORIGIN;
+const REQ_ORIGIN = process.env.MODE === 'development' ? DEV_ORIGIN : PRO_ORIGIN;
 
-const API_BASEURL = `${origin}/api/v1`;
+const API_BASEURL = `${REQ_ORIGIN}/api/v1`;
 
 export const apiBase = async (path: string, query?: string) => {
   const res = await fetch(`${API_BASEURL}/${path}?${query}`);
