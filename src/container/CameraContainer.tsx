fab3ebd9474cd349c6afadab0ced3fdd1d17bc6e
Wataru Katsuki <a17dc050@dhw.ac.jp>
2021-02-09 15:57:21 +0900

    fix: Fix it so that it can be combined into one group and displayed in SP

--- src/container/CameraContainer.tsx
+++ src/container/CameraContainer.tsx
@@ -1,21 +1,33 @@
-import React, { memo, useEffect, useRef, useState } from 'react';
+import React, { memo, useEffect, useMemo, useRef, useState } from 'react';
 import { useRouteMatch } from 'react-router';
 import { WrappedCanvas } from '../components/CanvasRef';
 import { fetchUserFavorites } from '../lib/api';
 import { useMarkerContext, useUserContext } from '../lib/context';
 import { useAnimationFrame, useArToolkitInit, useTextLoader, useWebGLRenderer } from '../utils';
 
-export const sceneLeft = new THREE.Scene();
-export const sceneRight = new THREE.Scene();
-export const groupLeft = new THREE.Group();
-export const groupRight = new THREE.Group();
+export const REFT = ['<'];
+export const RIGHT = ['>'];
+
+// export const sceneLeft = new THREE.Scene();
+// export const sceneRight = new THREE.Scene();
+export const sceneCenter = new THREE.Scene();
+
+// export const groupLeft = new THREE.Group();
+// export const groupRight = new THREE.Group();
+export const groupCenter = new THREE.Group();
+
 export const perspectiveCamera = new THREE.PerspectiveCamera();
 export const mouse = new THREE.Vector3();
 
-sceneLeft.add(perspectiveCamera);
-sceneRight.add(perspectiveCamera);
-sceneLeft.add(groupLeft);
-sceneRight.add(groupRight);
+// sceneLeft.add(perspectiveCamera);
+// sceneRight.add(perspectiveCamera);
+sceneCenter.add(perspectiveCamera);
+
+// sceneLeft.add(groupLeft);
+// sceneRight.add(groupRight);
+sceneCenter.add(groupCenter);
+// sceneCenter.add(groupLeft);
+// sceneCenter.add(groupRight);
 
 export const CameraContainer = memo(() => {
   const { userContext } = useUserContext();
@@ -25,42 +37,56 @@ export const CameraContainer = memo(() => {
     params: { screenName },
   } = useRouteMatch<{ screenName: string }>();
 
-  const canvasLeftRef = useRef<HTMLCanvasElement | null>(null);
-  const canvasRightRef = useRef<HTMLCanvasElement | null>(null);
+  // const canvasLeftRef = useRef<HTMLCanvasElement | null>(null);
+  // const canvasRightRef = useRef<HTMLCanvasElement | null>(null);
+  const canvasCenterRef = useRef<HTMLCanvasElement | null>(null);
 
-  const webGLRendererLeft = useWebGLRenderer({
-    canvas: canvasLeftRef,
-    width: window.innerWidth / 4,
+  // const webGLRendererLeft = useWebGLRenderer({
+  //   canvas: canvasLeftRef,
+  //   width: window.innerWidth / 4,
+  //   stylePosition: 'left',
+  // });
+  // const webGLRendererRight = useWebGLRenderer({
+  //   canvas: canvasRightRef,
+  //   width: window.innerWidth / 4,
+  //   stylePosition: 'right',
+  // });
+  const webGLRendererCenter = useWebGLRenderer({
+    canvas: canvasCenterRef,
+    width: window.innerWidth,
     stylePosition: 'left',
   });
-  const webGLRendererRight = useWebGLRenderer({
-    canvas: canvasRightRef,
-    width: window.innerWidth / 4,
-    stylePosition: 'right',
-  });
-  const { arToolkitContext: arToolkitContextLeft, arToolkitSource: arToolkitSourceLeft } = useArToolkitInit(
-    webGLRendererLeft,
-  );
-  const { arToolkitContext: arToolkitContextRight, arToolkitSource: arToolkitSourceRight } = useArToolkitInit(
-    webGLRendererRight,
-  );
-  const [res, setRes] = useState<TwitterUserFavorite[] | null>(null);
 
-  console.log(res);
+  // const { arToolkitContext: arToolkitContextLeft, arToolkitSource: arToolkitSourceLeft } = useArToolkitInit(
+  //   webGLRendererLeft,
+  // );
+  // const { arToolkitContext: arToolkitContextRight, arToolkitSource: arToolkitSourceRight } = useArToolkitInit(
+  //   webGLRendererRight,
+  // );
+  const { arToolkitContext: arToolkitContextCenter, arToolkitSource: arToolkitSourceCenter } = useArToolkitInit(
+    webGLRendererCenter,
+  );
+
+  const [res, setRes] = useState<TwitterUserFavorite[] | null>(null);
 
   useEffect(() => {
     getUser(screenName);
     if (!patternURL) return;
-    new THREEx.ArMarkerControls(arToolkitContextRight, groupRight, {
+    new THREEx.ArMarkerControls(arToolkitContextCenter, groupCenter, {
       type: 'pattern',
       patternUrl: patternURL,
       changeMatrixMode: 'modelViewMatrix',
     });
-    new THREEx.ArMarkerControls(arToolkitContextLeft, groupLeft, {
-      type: 'pattern',
-      patternUrl: patternURL,
-      changeMatrixMode: 'modelViewMatrix',
-    });
+    // new THREEx.ArMarkerControls(arToolkitContextRight, groupRight, {
+    //   type: 'pattern',
+    //   patternUrl: patternURL,
+    //   changeMatrixMode: 'modelViewMatrix',
+    // });
+    // new THREEx.ArMarkerControls(arToolkitContextLeft, groupLeft, {
+    //   type: 'pattern',
+    //   patternUrl: patternURL,
+    //   changeMatrixMode: 'modelViewMatrix',
+    // });
   }, [screenName, patternURL]);
 
   useEffect(() => {
@@ -73,50 +99,69 @@ export const CameraContainer = memo(() => {
     getUserFavorites();
   }, [user]);
 
-  const { textLoader: textLoaderLeft } = useTextLoader({ test: ['<'] });
-  const { textLoader: textLoaderRight } = useTextLoader({ test: ['>'] });
+  const contentsText = useMemo(() => {
+    if (!res) return [''];
+    const httpsIndex = res[0].text.indexOf('https');
+    const text = res[0].text.slice(0, httpsIndex);
+    const resultText = text.split(/(.{10})/).filter((e) => e);
+    return resultText;
+  }, [res]);
+
+  const { result: textLoaderCenter } = useTextLoader({ test: contentsText });
+  const { result: textLoaderLeft } = useTextLoader({ test: REFT });
+  const { result: textLoaderRight } = useTextLoader({ test: RIGHT });
+
+  // console.log(textLoaderLeft);
+
+  // useEffect(() => {
+  //   const handleClick = (event: MouseEvent, string: string) => {
+  //     console.log(string);
+  //     const element = event.target;
+  //     if (!(element instanceof HTMLCanvasElement)) return;
+  //     const rect = element.getBoundingClientRect();
+  //     const x = event.clientX - rect.left;
+  //     const y = event.clientY - rect.top;
+
+  //     const mouseX = (x / element.offsetWidth) * 2 - 1;
+  //     const mouseY = -(y / element.offsetHeight) * 2 + 1;
+
+  //     mouse.unproject(perspectiveCamera);
+  //     mouse.set(mouseX, mouseY, 0);
+  //   };
+
+  //   if (!webGLRendererLeft) return;
+  //   if (!webGLRendererRight) return;
+  //   webGLRendererLeft.domElement.addEventListener('click', (e) => handleClick(e, 'webGLRendererLeft'));
+  //   webGLRendererRight.domElement.addEventListener('click', (e) => handleClick(e, 'webGLRendererRight'));
+  //   return () => {
+  //     webGLRendererLeft.domElement.removeEventListener('click', (e) => handleClick(e, 'webGLRendererLeft'));
+  //     webGLRendererRight.domElement.removeEventListener('click', (e) => handleClick(e, 'webGLRendererRight'));
+  //   };
+  // }, [webGLRendererLeft, webGLRendererRight]);
 
   useEffect(() => {
-    const handleClick = (event: MouseEvent, string: string) => {
-      console.log(string);
-      const element = event.target;
-      if (!(element instanceof HTMLCanvasElement)) return;
-      const rect = element.getBoundingClientRect();
-      const x = event.clientX - rect.left;
-      const y = event.clientY - rect.top;
-
-      const mouseX = (x / element.offsetWidth) * 2 - 1;
-      const mouseY = -(y / element.offsetHeight) * 2 + 1;
-
-      mouse.unproject(perspectiveCamera);
-      mouse.set(mouseX, mouseY, 0);
-    };
-
-    if (!webGLRendererLeft) return;
-    if (!webGLRendererRight) return;
-    webGLRendererLeft.domElement.addEventListener('click', (e) => handleClick(e, 'webGLRendererLeft'));
-    webGLRendererRight.domElement.addEventListener('click', (e) => handleClick(e, 'webGLRendererRight'));
-    return () => {
-      webGLRendererLeft.domElement.removeEventListener('click', (e) => handleClick(e, 'webGLRendererLeft'));
-      webGLRendererRight.domElement.removeEventListener('click', (e) => handleClick(e, 'webGLRendererRight'));
-    };
-  }, [webGLRendererLeft, webGLRendererRight]);
+    groupCenter.add(textLoaderRight);
+  }, [textLoaderRight]);
 
   useEffect(() => {
-    groupLeft.add(textLoaderLeft);
-    groupRight.add(textLoaderRight);
-  }, [textLoaderLeft, textLoaderRight]);
+    groupCenter.add(textLoaderLeft);
+  }, [textLoaderLeft]);
+
+  useEffect(() => {
+    groupCenter.add(textLoaderCenter);
+  }, [textLoaderCenter]);
 
   useAnimationFrame({
-    arToolkitContext: [arToolkitContextLeft, arToolkitContextRight],
-    arToolkitSource: [arToolkitSourceLeft, arToolkitSourceRight],
-    webGLRenderer: [webGLRendererLeft, webGLRendererRight],
+    arToolkitContext: [arToolkitContextCenter],
+    arToolkitSource: [arToolkitSourceCenter],
+    webGLRenderer: [webGLRendererCenter],
   });
 
   return (
     <div>
-      <WrappedCanvas ref={canvasLeftRef} />
-      <WrappedCanvas ref={canvasRightRef} />
+      <WrappedCanvas ref={canvasCenterRef} />
+      {/* <WrappedCanvas ref={canvasLeftRef} />
+      <WrappedCanvas ref={canvasRightRef} /> */}
     </div>
   );
 });
--- src/utils/useAnimation.tsx
+++ src/utils/useAnimation.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react';
-import { mouse, perspectiveCamera, sceneRight, sceneLeft } from '../container';
+import { mouse, perspectiveCamera, sceneCenter } from '../container';
 import { ArToolkitContext, ArToolkitSource } from '../types/THREEx';
 
 interface Props {
@@ -16,16 +16,22 @@ export const useAnimationFrame = ({ arToolkitSource, arToolkitContext, webGLRend
     const animate = () => {
       if (arToolkitSource[0].ready) {
         arToolkitContext[0].update(arToolkitSource[0].domElement);
-        sceneLeft.visible = perspectiveCamera.visible;
+        sceneCenter.visible = perspectiveCamera.visible;
       }
-      if (arToolkitSource[1].ready) {
-        arToolkitContext[1].update(arToolkitSource[1].domElement);
-        sceneRight.visible = perspectiveCamera.visible;
-      }
+      // if (arToolkitSource[1].ready) {
+      //   arToolkitContext[1].update(arToolkitSource[1].domElement);
+      //   sceneRight.visible = perspectiveCamera.visible;
+      // }
+      // if (arToolkitSource[2].ready) {
+      //   arToolkitContext[2].update(arToolkitSource[1].domElement);
+      //   sceneCenter.visible = perspectiveCamera.visible;
+      // }
       if (!webGLRenderer[0]) return;
-      if (!webGLRenderer[1]) return;
-      webGLRenderer[0].render(sceneLeft, perspectiveCamera);
-      webGLRenderer[1].render(sceneRight, perspectiveCamera);
+      // if (!webGLRenderer[1]) return;
+      // if (!webGLRenderer[2]) return;
+      webGLRenderer[0].render(sceneCenter, perspectiveCamera);
+      // webGLRenderer[1].render(sceneRight, perspectiveCamera);
+      // webGLRenderer[2].render(sceneCenter, perspectiveCamera);
       requestRef.current = requestAnimationFrame(animate);
 
       raycaster.setFromCamera(mouse, perspectiveCamera);
@@ -34,5 +40,5 @@ export const useAnimationFrame = ({ arToolkitSource, arToolkitContext, webGLRend
     };
     requestRef.current = requestAnimationFrame(animate);
     return () => cancelAnimationFrame(requestRef.current);
-  }, [webGLRenderer, sceneLeft, sceneRight, perspectiveCamera, mouse]);
+  }, [webGLRenderer, sceneCenter, perspectiveCamera, mouse]);
 };
